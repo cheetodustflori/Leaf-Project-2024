be8075d483fcbd7d6eec60199eb1e6d2c10e6c5e
cheetodustflori <florianneche@gmail.com>
2024-09-07 23:51:02 -0500

    cleaned up folders

--- Leaf Project/ViewModels/ProfileViewViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileViewViewModel.swift
-//  practice 2
-//
-//  Created by Quynh Tram on 11/15/23.
-//
-
-import Foundation
--- Leaf Project/model/Leaf.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Leaf.swift
-//  practice 2
-//
-//  Created by Quynh Tram on 11/4/23.
-//
-
-import Foundation
